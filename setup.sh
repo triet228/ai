144005e2a77be178d5e1838ebf417f2fcb80046f
Triet Ho <hominhtriet2004@gmail.com>
2026-08-12 19:17:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -114,6 +114,7 @@ docker run -d \
   -v open-webui:/app/backend/data \
   -e OLLAMA_BASE_URL=http://127.0.0.1:11434 \
   -e WEBUI_AUTH=false \
+  -e ENABLE_API_KEY=false \
   -e ENABLE_OLLAMA_TOOLS=false \
   -e ENABLE_FUNCTION_CALLING=false \
   -e ENABLE_QUEUE=true \
